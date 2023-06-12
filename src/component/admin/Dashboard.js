84a4d720c418e729b7728e6e2cc301c4f2a91a52
TungSLTR <98029552+TungSLTR@users.noreply.github.com>
2023-06-12 18:29:57 +0700

    sd

--- src/component/admin/Dashboard.js
+++ src/component/admin/Dashboard.js
@@ -1,49 +1,27 @@
-import { Card, Col, Row, Statistic, Typography, List, Tooltip } from "antd";
+import {
+  Card,
+  Col,
+  Row,
+  Statistic,
+  Typography,
+  List,
+  Tooltip,
+  Pagination,Form, Button, Input
+} from "antd";
 import { Pie } from "@ant-design/charts";
 import { Comment } from "@ant-design/compatible";
+import React, { useState, useEffect } from "react";
 
-const data = [
-  { type: "Product A", value: 30 },
-  { type: "Product B", value: 50 },
-  { type: "Product C", value: 20 },
-];
-const datareview = [
-  {
-    actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-    author: "Han Solo",
-    avatar: "https://xsgames.co/randomusers/assets/avatars/pixel/28.jpg",
-    content: (
-      <p>
-        We supply a series of design principles, practical patterns and high
-        quality design resources (Sketch and Axure), to help people create their
-        product prototypes beautifully and efficiently.
-      </p>
-    ),
-    datetime: (
-      <Tooltip title="2016-11-22 11:22:33">
-        <span>8 hours ago</span>
-      </Tooltip>
-    ),
-  },
-  {
-    actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-    author: "Han Solo",
-    avatar: "https://xsgames.co/randomusers/assets/avatars/pixel/37.jpg",
-    content: (
-      <p>
-        We supply a series of design principles, practical patterns and high
-        quality design resources (Sketch and Axure), to help people create their
-        product prototypes beautifully and efficiently.
-      </p>
-    ),
-    datetime: (
-      <Tooltip title="2016-11-22 10:22:33">
-        <span>9 hours ago</span>
-      </Tooltip>
-    ),
-  },
-];
+import axios from "axios";
+import { API_URL } from "../../config";
+
 const PieChart = () => {
+  const data = [
+    { type: "Product A", value: 30 },
+    { type: "Product B", value: 50 },
+    { type: "Product C", value: 20 },
+  ];
+
   const config = {
     appendPadding: 10,
     data,
@@ -63,65 +41,172 @@ const PieChart = () => {
   return <Pie {...config} />;
 };
 
-const Dashboard = () => (
-  <>
-    <Row gutter={16}>
-      <Col span={8}>
-        <Card>
-          <Statistic title="Tổng doanh thu" value={1000000} suffix="VNĐ" />
-          <Typography.Text type="secondary">
-            Doanh thu hôm nay: 500000 VNĐ
-          </Typography.Text>
-        </Card>
-      </Col>
-      <Col span={8}>
-        <Card>
-          <Statistic title="Số người xem" value={5000} />
-          <Typography.Text type="secondary">
-            Lượt xem hôm nay: 5
-          </Typography.Text>
-        </Card>
-      </Col>
-      <Col span={8}>
-        <Card>
-          <Statistic title="Số lượng thanh toán" value={10} />
-          <Typography.Text type="secondary">
-            Số lượng thanh toán hôm nay: 5
-          </Typography.Text>
-        </Card>
-      </Col>
-    </Row>
-    <Row gutter={16} style={{ marginTop: 16 }}>
-      <Col span={12}>
-        <Card>
-          <Typography.Title level={4}>Biểu đồ sản phẩm đã bán</Typography.Title>
-          <PieChart />
-        </Card>
-      </Col>
-      <Col span={12}>
-        <Card>
-          <Typography.Title level={4}>Đánh giá bình luận</Typography.Title>
-          <List
-            className="comment-list"
-            header={`${datareview.length} replies`}
-            itemLayout="horizontal"
-            dataSource={datareview}
-            renderItem={(item) => (
-              <li>
-                <Comment
-                  actions={item.actions}
-                  author={item.author}
-                  avatar={item.avatar}
-                  content={item.content}
-                  datetime={item.datetime}
-                />
-              </li>
-            )}
-          />
-        </Card>
-      </Col>
-    </Row>
-  </>
-);
+const Dashboard = () => {
+  const [review, setReview] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(3);
+  const [replyContent, setReplyContent] = useState("");
+  const [selectedReview, setSelectedReview] = useState(null);
+  const [selectedCommentIndex, setSelectedCommentIndex] = useState(null);
+
+
+  useEffect(() => {
+    getReview();
+  }, []);
+
+  const getReview = async () => {
+    try {
+      const response = await axios.get(`${API_URL}admin/review/dashboard`);
+      setReview(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const currentReview = review.slice(startIndex, endIndex);
+
+  const handlePageChange = (page, pageSize) => {
+    setCurrentPage(page);
+  };
+
+  const handleReplyChange = (e) => {
+    setReplyContent(e.target.value);
+  };
+
+  const handleReplySubmit = async () => {
+    if (selectedReview) {
+      const { makh, masp } = selectedReview;
+      try {
+        await axios.patch(`${API_URL}admin/review/dashboard/${makh}/${masp}`, {
+          reply: replyContent,
+        });
+        setReplyContent("");
+        setSelectedReview(null);
+        getReview();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleReplyCancel = () => {
+    setReplyContent("");
+    setSelectedReview(null);
+  };
+
+  const handleReply = (review, index) => {
+    setReplyContent("");
+    setSelectedReview(review);
+    setSelectedCommentIndex(index);
+  };
+  
+
+  return (
+    <>
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card>
+            <Statistic title="Tổng doanh thu" value={1000000} suffix="VNĐ" />
+            <Typography.Text type="secondary">
+              Doanh thu hôm nay: 500000 VNĐ
+            </Typography.Text>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card>
+            <Statistic title="Số người xem" value={5000} />
+            <Typography.Text type="secondary">
+              Lượt xem hôm nay: 5
+            </Typography.Text>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card>
+            <Statistic title="Số lượng thanh toán" value={10} />
+            <Typography.Text type="secondary">
+              Số lượng thanh toán hôm nay: 5
+            </Typography.Text>
+          </Card>
+        </Col>
+      </Row>
+      <Row gutter={16} style={{ marginTop: 16 }}>
+        <Col span={12}>
+          <Card>
+            <Typography.Title level={4}>
+              Biểu đồ sản phẩm đã bán
+            </Typography.Title>
+            <PieChart />
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card>
+            <Typography.Title level={4}>Đánh giá bình luận</Typography.Title>
+            <List
+  className="comment-list"
+  header={`${review.length} đánh giá`}
+  itemLayout="horizontal"
+  dataSource={currentReview}
+  renderItem={(item, index) => (
+    <li>
+      <Comment
+        actions={[
+          <Button
+            key={`comment-list-reply-to-${index}`}
+            onClick={() => handleReply(item, index)}
+          >
+            Reply to
+          </Button>,
+        ]}
+        author={`Đánh giá của ${item.user.tenkh} về sản phẩm ${item.sanpham.tensp}`}
+        avatar="https://ibiettuot.com/wp-content/uploads/2021/10/avatar-mac-dinh.png"
+        content={item.noidung}
+        datetime={item.createAt}
+      />
+      {item.reply && (
+        <Comment
+          style={{ marginLeft: 32 }}
+          author="Admin"
+          avatar="https://cdn-icons-png.flaticon.com/256/1930/1930026.png"
+          content={item.reply}
+          datetime={item.replyAt}
+        />
+      )}
+      {selectedReview && selectedCommentIndex === index && (
+        <Form>
+          <Form.Item>
+            <Input.TextArea
+              value={replyContent}
+              onChange={handleReplyChange}
+              placeholder="Reply content"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" onClick={handleReplySubmit}>
+              Reply
+            </Button>{" "}
+            <Button onClick={handleReplyCancel}>Cancel</Button>
+          </Form.Item>
+        </Form>
+      )}
+    </li>
+  )}
+/>
+
+
+            <Pagination
+              current={currentPage}
+              pageSize={pageSize}
+              total={review.length}
+              onChange={handlePageChange}
+            />
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default Dashboard;
